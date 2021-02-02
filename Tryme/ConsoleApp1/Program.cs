b95c5247579f0c5c7deae694f45037721505eac9
TravaLuL <77954239+TravaLuL@users.noreply.github.com>
2021-02-02 14:44:54 +0400

    Add files via upload

---
+++ Tryme/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите Первое число :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Напишите Второе число :");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Напишите Третье число :");
+            int z = Convert.ToInt32(Console.ReadLine());
+            int summa = (a + b + z);
+            Console.WriteLine("Сумма чисел = " + summa);
+            int multiply = (a * b * z);
+            Console.WriteLine("Умножение чисел = " + multiply);
+
+
+
+
+        }
+    }
+}
---
+++ Tryme/Tryme/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tryme
+{
+    class Sred
+    {
+        static void Main(string[] args)
+        {
+
+            int delenie = 2;
+
+
+            Console.WriteLine("Напишите первое число :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Напишите Второе число :");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sum = (a + b) / delenie;
+            Console.WriteLine("Среднее арифметическое чисел = " + sum);
+
+
+
+        }
+    }
+    
+  
+}
+
